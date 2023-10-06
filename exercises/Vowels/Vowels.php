f8730e588afd01759664f7fd3fae41bd71c0d38c
AKV85 <a.kotov.laknojus@gmail.com>
2023-10-06 13:41:42 +0300

    Vowels and tests

--- exercises/Vowels/Vowels.php
+++ exercises/Vowels/Vowels.php
@@ -13,4 +13,71 @@ namespace Exercises\Vowels;
  */
 final class Vowels
 {
+    public static function count(string $string)
+    {
+        $pattern = '/[euioaEIUOA]/';
+        return preg_match_all($pattern, $string);
+    }
+
+    public static function count1(string $string): int
+    {
+        return strlen(preg_replace('#[^aeiou]#i', '', $string));
+    }
+
+    public static function count2(string $string): int
+    {
+        $counter = 0;
+
+        foreach (str_split(strtolower($string)) as $char) {
+            if (strpos('aeiou', $char) === false) {
+                continue;
+            }
+
+            ++$counter;
+        }
+
+        return $counter;
+    }
+
+    public static function count3(string $string): int
+    {
+        $counter = 0;
+        $length = strlen($string);
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        for ($i = 0; $i < $length; ++$i) {
+            if (in_array(strtolower($string[$i]), $vowels, true)) {
+                ++$counter;
+            }
+        }
+
+        return $counter;
+    }
 }
+
+$string=' hello_World and people all there ! What\'s up, Doc?This text isn\'t very huge';
+
+$startTimeVowels = microtime(true);
+Vowels::count($string);
+$endTimeVowels = microtime(true);
+$executionTimeVowels = $endTimeVowels - $startTimeVowels; //0.000025
+echo "Execution time for the count method: $executionTimeVowels seconds\n";
+
+$startTimeVowels = microtime(true);
+Vowels::count1($string);
+$endTimeVowels = microtime(true);
+$executionTimeVowels = $endTimeVowels - $startTimeVowels; //0.000004  winner
+echo "Execution time for the count1 method: $executionTimeVowels seconds\n";
+
+$startTimeVowels = microtime(true);
+Vowels::count2($string);
+$endTimeVowels = microtime(true);
+$executionTimeVowels = $endTimeVowels - $startTimeVowels; //0.000016
+echo "Execution time for the count2 method: $executionTimeVowels seconds\n";
+
+$startTimeVowels = microtime(true);
+Vowels::count3($string);
+$endTimeVowels = microtime(true);
+$executionTimeVowels = $endTimeVowels - $startTimeVowels; //0.000028
+echo "Execution time for the count3 method: $executionTimeVowels seconds\n";
+
--- tests/Vowels/VowelsTest.php
+++ tests/Vowels/VowelsTest.php
@@ -12,34 +12,46 @@ final class VowelsTest extends TestCase
 {
     public function testHasCount(): void
     {
-        self::markTestSkipped();
+        // self::markTestSkipped();
         self::assertTrue(
             method_exists(Vowels::class, 'count'),
             'Class does not have static method count'
+        );
+        self::assertTrue(
+            method_exists(Vowels::class, 'count1'),
+            'Class does not have static method count1'
+        );
+        self::assertTrue(
+            method_exists(Vowels::class, 'count2'),
+            'Class does not have static method count2'
+        );
+        self::assertTrue(
+            method_exists(Vowels::class, 'count3'),
+            'Class does not have static method count3'
         );
     }
 
     public function testCanCount(): void
     {
-        self::markTestSkipped();
-        self::assertSame(5, Vowels::count('abcdefghijklmnopqrstuvwxyz'));
+        // self::markTestSkipped();
+        self::assertSame(6, Vowels::count('abcdefghijklmnopqrstuvwxyz  a'));
     }
 
     public function testCanCountAllUpper(): void
     {
-        self::markTestSkipped();
-        self::assertSame(5, Vowels::count('AEIOU'));
+        // self::markTestSkipped();
+        self::assertSame(5, Vowels::count1(' !AEIOU'));
     }
 
     public function testCanCountOnly(): void
     {
-        self::markTestSkipped();
-        self::assertSame(5, Vowels::count('aeiou'));
+        // self::markTestSkipped();
+        self::assertSame(5, Vowels::count2('aeiou !'));
     }
 
     public function testCanCountNone(): void
     {
-        self::markTestSkipped();
-        self::assertSame(0, Vowels::count('bcdfghjkl'));
+        // self::markTestSkipped();
+        self::assertSame(0, Vowels::count3('bcdfghjkl !'));
     }
 }
